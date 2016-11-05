5576167e5769266087cfa64e31ed68b944d779b2
Chenggong Lyu <lcgong@gmail.com>
2016-11-05 11:31:12 +0800

    可以标记多个答案

--- app/comp/markdown/parseSolution.js
+++ app/comp/markdown/parseSolution.js
@@ -1,11 +1,11 @@
-'use strict';
 
 // (O) (V) U+2714 ✔ heavy check mark
 // (X)     U+2718 ✘ HEAVY BALLOT X (bold cross)
 
+
 var markerStr = '%%%';
 
-function parseSolution(state, startLine, endLine, silent) {
+export function parseSolution(state, startLine, endLine, silent) {
   var pos, nextLine, marker_count, markup, params, token,
     old_parent, old_line_max,
     auto_closed = false,
@@ -19,12 +19,7 @@ function parseSolution(state, startLine, endLine, silent) {
   pos = start + markerStr.length;
 
   var paramsStr = state.src.slice(pos, max);
-  var resolvedOptions = [];
-  var matched;
-  var optionPattern = /\(([0-9a-zA-Z]+)\)/g;
-  while ((matched = optionPattern.exec(paramsStr)) != null) {
-    resolvedOptions.push(matched[1]);
-  }
+  var resolvedOptions = parseBlankSolution(paramsStr);
 
   // Since start is found, we can report success here in validation mode
   if (silent) {
@@ -66,7 +61,10 @@ function parseSolution(state, startLine, endLine, silent) {
   token.block = true;
   token.info = resolvedOptions;
   token.map = [startLine, nextLine];
-  token.attrPush(['solution', resolvedOptions]);
+  // token.attrPush(['solution', resolvedOptions]);
+  token.meta = {
+    solution: resolvedOptions
+  }
 
   state.md.block.tokenize(state, startLine + 1, nextLine);
 
@@ -78,7 +76,7 @@ function parseSolution(state, startLine, endLine, silent) {
   state.lineMax = old_line_max;
 
 
-  console.log('sol', state.line, nextLine);
+  // console.log('sol', state.line, nextLine);
 
   state.line = nextLine; // + (auto_closed ? 1 : 0);
 
@@ -86,4 +84,45 @@ function parseSolution(state, startLine, endLine, silent) {
 }
 
 
-module.exports = parseSolution;
+/**
+ * 解析下列
+ *  1. (A) (B) (C) ; 2. (a) (c)
+ *  1. (A) (B) (C)  2. (a) (c)
+ *  (A) (B) (C) ; 2. (a) (c)
+ *  (A) (B) (C) ; (a) (c)
+ * 返回每组答案 [[1, A, B, C], [2, a, c]]
+ * 如果没有题号用null表示  [[A, B, C], [a, c]]
+ */
+function parseBlankSolution(paramsStr) {
+  let optionPattern = /([1-9][0-9]*)[\.、]?|[\(（]([a-zA-Z]+)[）\)]|([;；。])/g;
+
+  let resolvedOptions = [];
+  let blankSolution = [null];
+  let matched;
+  while ((matched = optionPattern.exec(paramsStr)) != null) {
+    if (matched[3]) { // 分号间隔
+      if (blankSolution.length > 1 || blankSolution[0] != null) {
+        resolvedOptions.push(blankSolution);
+      }
+
+      blankSolution = [null];
+
+    } else if (matched[1]) {
+      if (blankSolution.length > 1 || blankSolution[0] != null) {
+        resolvedOptions.push(blankSolution);
+      }
+
+      blankSolution = [matched[1]];
+    } else {
+      if (matched[2]) {
+        blankSolution.push(matched[2]);
+      }
+    }
+  }
+
+  if (blankSolution.length > 1 || blankSolution[0] != null) {
+    resolvedOptions.push(blankSolution);
+  }
+
+  return resolvedOptions;
+}
