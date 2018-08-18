528a8a484d6c57adc86424924d803d56826d4657
Chenggong Lyu <lcgong@gmail.com>
2018-08-18 10:14:51 +0800

    删除一些废弃的程序

--- webapp/dcone/test/im.js
+++ webapp/dcone/test/im.js
@@ -5,6 +5,10 @@ import { Subject } from "rxjs";
 
 
 test("diff a list", () => {
+    let objs = Immutable.List([10,20,30,40]);
 
+    console.log([...objs]);
 
+    let iter = objs[Symbol.iterator]();
+    console.log(iter.next());
 });
